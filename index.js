eb5145e25ff317959bed037af58f3e7be810ade6
Charles Pick <charles.pick@gmail.com>
2013-03-31 20:17:37 +0100

    Added tag v2.2.0

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function(){
   var jQuery = require('jquery');
   /* =============================================================
-   * bootstrap-collapse.js v2.1.1
+   * bootstrap-collapse.js v2.2.0
    * http://twitter.github.com/bootstrap/javascript.html#collapse
    * =============================================================
    * Copyright 2012 Twitter, Inc.
@@ -145,16 +145,14 @@ function(){
    /* COLLAPSIBLE DATA-API
     * ==================== */
   
-    $(function () {
-      $('body').on('click.collapse.data-api', '[data-toggle=collapse]', function (e) {
-        var $this = $(this), href
-          , target = $this.attr('data-target')
-            || e.preventDefault()
-            || (href = $this.attr('href')) && href.replace(/.*(?=#[^\s]+$)/, '') //strip for ie7
-          , option = $(target).data('collapse') ? 'toggle' : $this.data()
-        $this[$(target).hasClass('in') ? 'addClass' : 'removeClass']('collapsed')
-        $(target).collapse(option)
-      })
+    $(document).on('click.collapse.data-api', '[data-toggle=collapse]', function (e) {
+      var $this = $(this), href
+        , target = $this.attr('data-target')
+          || e.preventDefault()
+          || (href = $this.attr('href')) && href.replace(/.*(?=#[^\s]+$)/, '') //strip for ie7
+        , option = $(target).data('collapse') ? 'toggle' : $this.data()
+      $this[$(target).hasClass('in') ? 'addClass' : 'removeClass']('collapsed')
+      $(target).collapse(option)
     })
   
   }(jQuery);
