f7d383a4e98442d7d6da60454fed438ad6743f62
Charles Pick <charles.pick@gmail.com>
2013-03-31 20:17:38 +0100

    Added tag v2.2.1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function(){
   var jQuery = require('jquery');
   /* =============================================================
-   * bootstrap-collapse.js v2.2.0
+   * bootstrap-collapse.js v2.2.1
    * http://twitter.github.com/bootstrap/javascript.html#collapse
    * =============================================================
    * Copyright 2012 Twitter, Inc.
